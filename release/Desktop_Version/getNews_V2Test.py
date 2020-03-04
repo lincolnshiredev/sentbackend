d525cd38e521be22166127438acc0280952722b8
lincolnshiredev <56600751+lincolnshiredev@users.noreply.github.com>
2020-03-04 06:05:45 +0000

    moved to release
    verified by CK and AD

---
+++ release/Desktop_Version/getNews_V2Test.py
@@ -0,0 +1,149 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from datetime import datetime, timedelta
+from newsapi import NewsApiClient
+import pandas as pd
+import dateparser
+import requests
+import string
+import time
+import json
+# https://us-central1-enhanced-bebop-268815.cloudfunctions.net/getArticles?ticker=
+
+sia = SentimentIntensityAnalyzer()
+
+date = (datetime.now()).strftime("%Y-%m-%d")# - timedelta(days=1)
+
+newsapi = NewsApiClient(api_key='d1303aa27f3840d9a0c5da1cccfc171b')
+headers = {'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+           'x-rapidapi-key': "8d02ef92a9mshbe032b4fec7a9bfp15eb07jsn6fc9e511caff"}
+
+def getNewsApiArticles(ticker: str, date):
+    
+    print('Getting Artices from News API')
+    newsApiDf = newsapi.get_everything(q=ticker,
+                                          from_param=date,
+                                          language='en',
+                                          page=1)
+
+    newsApiDf = pd.DataFrame(newsApiDf)
+    newsApiDf = pd.concat([newsApiDf.drop(
+        ['articles'], axis=1), newsApiDf['articles'].apply(pd.Series)], axis=1)
+    
+    newsApiDf.drop_duplicates(subset="title",
+                                 keep=False, inplace=True)
+    
+    for index, row in newsApiDf.iterrows():
+        time1 =  dateparser.parse(str(row['publishedAt'])) 
+        time1 = int(time.mktime(time1.timetuple()))
+        time1 = time.gmtime(time1)
+        time1 = time.strftime("%Y-%m-%d", time1)
+        newsApiDf.loc[index, 'publishedAt'] = time1
+        #newsApiDf = newsApiDf[newsApiDf['publishedAt'] == date] # this overrides the time, do we want that?
+
+    for index, row in newsApiDf.iterrows():
+        newsApiDf.loc[index, 'sourceName'] = (
+            (((str(row['source']).split("'name': '", 1))[1]).split("'}", 1))[0])
+        
+    print("Successfully got articles from News API")
+
+    return newsApiDf
+
+
+def getYahooNewsArticles(ticker: str):
+    print('Getting Articles from Yahoo')
+    querystring = {"category": ticker, "region": "US"}
+
+    yahooNews = requests.get(url='https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-newsfeed',
+                        headers=headers, params=querystring)
+
+    yahooNewsDf = pd.read_json(json.dumps((yahooNews.json())['items']['result']))
+    yahooNewsDf = yahooNewsDf.drop(columns=['uuid', 'main_image','entities', 'ignore_main_image', 'streams',
+                          'offnet', 'reference_id', 'is_magazine', 'type', 'author'])
+    yahooNewsDf['summary'] = yahooNewsDf['summary'].str.lower()
+    yahooNewsDf['content'] = yahooNewsDf['content'].str.lower()
+
+    for index, row in yahooNewsDf.iterrows():
+        yahooNewsDf.loc[index, 'sentiment'] = sentiment(row['summary'])
+        yahooNewsDf.loc[index, 'urlToImage'] = "https://images.pexels.com/photos/102720/pexels-photo-102720.jpeg" 
+        # Example stock image, by: Markus Spiske, hosted on: https://www.pexels.com/
+    
+    yahooNewsDf = yahooNewsDf[yahooNewsDf['sentiment'] != 0]
+    
+    yahooNewsDf.rename(columns={'published_at': 'publishedAt',
+                        'summary': 'description', 'link': 'url','publisher':'sourceName'}, inplace=True)
+   
+    yahooNewsDf = yahooNewsDf.drop(columns=['content','description'])
+   
+    print("successfully retrieved articles from yahoo and performed sentiment analysis")
+    return yahooNewsDf
+
+
+def sentiment(text):
+    return sia.polarity_scores(text)['compound']
+
+
+def runSent(dataFrame):
+    print('Perfoming Sentiment Analysis on news api')
+    for index, row in dataFrame.iterrows():
+        if (row['description'] != None):
+            sentVal = sentiment(row['description'])
+            dataFrame.loc[index, 'sentiment'] = sentVal
+            sentVal = 0
+        else:
+            sentVal = sentiment(row['content'])
+            dataFrame.loc[index, 'sentiment'] = sentVal
+            sentVal = 0
+
+    dataFrame = dataFrame[dataFrame['sentiment'] != 0]
+    print('Sentiment Analysis Complete on news api')
+    return dataFrame
+
+
+newsApiDf = pd.DataFrame()
+yahooNewsDf = pd.DataFrame() 
+
+numberOfArticles:int = 0
+overallSentiment: float = 0.0
+
+tickers = ["aapl"]
+
+for ticker in tickers:
+    newsApiDf = newsApiDf.append(getNewsApiArticles(ticker, date), sort=True)
+    yahooNewsDf = yahooNewsDf.append(getYahooNewsArticles(ticker))
+
+
+for ind,row in yahooNewsDf.iterrows():
+    overallSentiment  = overallSentiment+row['sentiment']
+    numberOfArticles = numberOfArticles + 1
+
+
+if (len(newsApiDf) != 0):
+   
+    newsApiDf = runSent(newsApiDf)
+   
+    for ind, row in newsApiDf.iterrows():
+        overallSentiment = overallSentiment+row['sentiment']
+        numberOfArticles = numberOfArticles + 1
+    
+    newsApiDf = newsApiDf.drop(columns=['content','description','status','totalResults','author','source'])
+    newsApiDf = newsApiDf.append(yahooNewsDf,sort=True)
+    
+    avgSentiment:float = overallSentiment/numberOfArticles
+    valuesDict ={"numberOfArticles":numberOfArticles,"overallSentiment":overallSentiment,"avgSentiment": ("{0:.2f}".format(avgSentiment))}
+
+    newsApiDict = newsApiDf.to_dict(orient='records')
+
+    newsApiDf.drop
+
+    newsArticlesDict = {'articles ':newsApiDict,'additionalData':valuesDict}
+
+    jsonresp = json.dumps(newsArticlesDict, default=str)
+else:
+    print('no results for news api')
+    
+    yahooNewsDict = yahooNewsDf.to_dict(orient='records')
+    newsArticlesDict = {'articles':yahooNewsDict,'additionalData':valuesDict}
+
+    jsonresp = json.dumps(newsArticlesDict, default=str)
+
+print(jsonresp)
---
+++ release/Desktop_Version/getStockData.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests
+import json
+# https://us-central1-enhanced-bebop-268815.cloudfunctions.net/stockData?ticker=
+def requestProfile(ticker: str): 
+    # Gets company information as well as the current share price
+    stock= requests.get(url='https://financialmodelingprep.com/api/v3/company/profile/' + ticker)
+    return(pd.read_json(json.dumps(stock.json()))) 
+    # Converts the json data result into a Pandas Dataframe which is returned
+
+def requestData(ticker: str, days: int): 
+    # Gets historic open and close prices for the stock based on how many days in the past you wish to view
+    stock= requests.get(url='https://financialmodelingprep.com/api/v3/historical-price-full/' + ticker + '?timeseries=' + str(days))
+    return (pd.read_json(json.dumps(stock.json())))
+
+def request(ticker: str,days: int): 
+    # Calls the functions and then removes data that is not required 
+    companyInfoDf: pd.Dataframe = requestProfile(ticker)
+    historicDataDf: pd.DataFrame = requestData(ticker,days)
+   
+    historicDataDf = historicDataDf.drop(columns=['symbol'])
+    companyInfoDf = companyInfoDf.drop(columns=['symbol'])
+    companyInfoDf = companyInfoDf.drop(['beta','changes','changesPercentage','description','image','industry','lastDiv','mktCap','range','volAvg'])
+    
+    # Converts the dataframes to a dictionary and then to json which is returned
+    profileDict = companyInfoDf.to_dict()
+    historicDict = historicDataDf.to_dict(orient='records')
+    
+    combinedDict = {'companyData':profileDict,'data':historicDict}
+    
+    return json.dumps(combinedDict)
+    
+tickers: str = ['aapl','msft','googl','amzn']
+# Example tickers for companies, case insensitive
+
+for ticker in tickers:
+    print(request(ticker,7)) 
+    # Ticker then the number of days you wish to get historic data for
+    
